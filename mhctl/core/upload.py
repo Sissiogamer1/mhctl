ffa887a2dd5c07e94619b774140ffc6d513b21ce
Sissiogamer1 <sissiogamer1@proton.me>
2026-01-20 20:57:02 +0100

    Adjuisted functions for the new rewritten provider_info() Removed other debug strings

--- mhctl/core/upload.py
+++ mhctl/core/upload.py
@@ -10,14 +10,14 @@ def upload_pomf(file: str, provider: str):
             # REWRITE BETTER TEXT
             raise ValueError(f"{file} does not exist")
         file_size = file_path.stat().st_size
-        if file_size > int(provider_info[2]):
+        if file_size > int(str(provider_info[2])):
             raise ValueError(f"{file} is too big")
         with open(file_path, "rb") as f:
             from ..runtime.internal import get_xxh3
             xxh3_hash = get_xxh3(f)
             with httpx.Client(timeout=30.0) as client:
                 files = {"files[]": (file_path.name, f)}
-                response = client.post(provider_info[1], files=files)
+                response = client.post(str(provider_info[1]), files=files)
                 temp_response = response.json()
                 if response.status_code != 200: # VEDI SE ANZICHE' USARE ERROR CODE PUOI USARE "SUCCESS" DALL'ENDPOINT
                     raise RuntimeError(f"Upload failed: HTTP {response.status_code}")
